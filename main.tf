208a10c4a2a2957e2451f29ae97b897e81c8d1af
Dima-shima <shifrin1970@gmail.com>
2026-08-11 22:34:38 +0200

    Completed

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ resource "azurerm_storage_container" "example" {
 }
 
 resource "azurerm_storage_blob" "example" {
-  name                 = var.blob_name
+  name                   = var.blob_name
   storage_account_name   = azurerm_storage_account.example.name
   storage_container_name = azurerm_storage_container.example.name
-  type                 = "Block"
-  source               = data.archive_file.terraform_code.output_path
+  type                   = "Block"
+  source                 = data.archive_file.terraform_code.output_path
 }
 
 data "archive_file" "terraform_code" {
